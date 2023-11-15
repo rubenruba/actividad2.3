fa8700d76d72f0e72b59dc33369213c685e8d0db
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2023-11-15 10:35:42 +0100

    add css and js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector('button').addEventListener('click', function(){
+    alert('PORQUE PULSASTE EL BOTON');
+    window.open('./asno.jpg', '_blank', 'width=1000px, height=1000px' ) 
+})
